5b074c4a070f87084a2f91e3bca81f12b90fe0ed
Sam Noreaksey <sam.noreaksey@outlook.com>
2017-10-26 09:30:37 -0700

    delete

--- static/js/controllers/aboutController.js
+++
@@ -1,6 +0,0 @@
-app.controller('AboutController', ['$scope', 'ContentService', '$rootScope',
-function($scope, ContentService, $rootScope){
-    $scope.ads_images = $rootScope.ads_images;
-    $scope.content = $rootScope.contents.about ? $rootScope.contents.about : '';
-    $scope.loading = false;
-}]);
--- static/js/controllers/contactusController.js
+++
@@ -1,6 +0,0 @@
-app.controller('ContactusController', ['$scope', 'ContentService', '$rootScope',
-function($scope, ContentService, $rootScope){
-    $scope.ads_images = $rootScope.ads_images;
-    $scope.content = $rootScope.contents.contactus ? $rootScope.contents.contactus : '';
-    $scope.loading = false;
-}]);
