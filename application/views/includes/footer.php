80c42beb4a9b80ea1892f82fa3c57d3beff82da2
Sam Noreaksey <sam.noreaksey@outlook.com>
2017-10-30 03:21:54 -0700

    all

--- application/views/includes/footer.php
+++
@@ -1,6 +0,0 @@
-    <!--JS-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-</body>
-</html>
